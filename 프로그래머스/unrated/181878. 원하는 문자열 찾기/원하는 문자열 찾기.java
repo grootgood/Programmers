59f92581b9881875ed076ca5a69940960e218a10
Groot <113498148+grootgood@users.noreply.github.com>
2024-01-04 23:08:37 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 5.08 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = myString.toUpperCase().contains(pat.toUpperCase()) ? 1 : 0;
+        
+        return answer;
+    }
+}
