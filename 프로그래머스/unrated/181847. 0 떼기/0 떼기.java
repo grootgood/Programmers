6517f66a134112d69dd55484c88cc001d63bca4e
Groot <113498148+grootgood@users.noreply.github.com>
2024-01-10 12:24:10 +0900

    [level unrated] Title: 0 떼기, Time: 0.52 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int num = (int)Double.parseDouble(n_str);
+        answer = Integer.toString(num);
+        return answer;
+    }
+}
